1c8116794fde96064285a61d260cca468b64dc58
Leonardo Polon <lpolon@gmail.com>
2019-12-09 11:18:27 -0300

    basic game loop working

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,8 @@
+let requestId;
+
 const canvas = {
   element: document.getElementById('canvas'),
-  context: document.getElementById('canvas').getContext('2d')
+  context: document.getElementById('canvas').getContext('2d'),
 };
 
 const gameSetup = {
@@ -10,8 +12,40 @@ const gameSetup = {
   },
 
   build() {
-    this.setCanvasSize(480, 549);
+    this.setCanvasSize(720, 630);
   },
 };
 
+const loopControl = {
+  start() {
+    if (!requestId) {
+      requestId = window.requestAnimationFrame(gameLoop);
+      // return requestId;
+    }
+  },
+  stop() {
+    if (requestId) {
+      console.log('hello, stop');
+      window.cancelAnimationFrame(requestId);
+      requestId = undefined;
+    }
+  },
+  clear() {
+    console.log('hello, clear');
+    canvas.context.clearRect(0, 0, canvas.element.width, canvas.element.height)
+  },
+};
+
+
+function gameLoop() {
+  requestId = undefined;
+  console.log('updating...');
+  // do stuff
+
+  loopControl.start();
+}
+
 gameSetup.build();
+loopControl.start();
+setTimeout(loopControl.clear, 3000);
+setTimeout(loopControl.stop, 5000);
