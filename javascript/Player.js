27a7bda61230513d3ebdba92e20201b079309b61
Leonardo Polon <lpolon@gmail.com>
2019-12-09 16:40:21 -0300

    basic movement for player class

--- javascript/Player.js
+++ javascript/Player.js
@@ -1,3 +1,17 @@
 class Player extends Component {
-  
+  goLeft() {
+    this.posX += -10;
+  }
+
+  goRight() {
+    this.posX += 10;
+  }
+
+  goTop() {
+    this.posY += -10;
+  }
+
+  gotBot() {
+    this.posY += 10;
+  }
 }
