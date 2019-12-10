1e87a1d6799fc9b7b99feba09ebd923093a98bd1
Leonardo Polon <lpolon@gmail.com>
2019-12-10 14:27:28 -0300

    WIP. Blinking

--- javascript/Enemy.js
+++ javascript/Enemy.js
@@ -1,1 +1,23 @@
-class Enemy extends Component {}
+class Enemy extends Component {
+  constructor(context, width, height, posX = 10, posY = 200, color) {
+    super();
+    this.c = context;
+    this.width = width;
+    this.height = height;
+    this.posX = posX;
+    this.posY = posY;
+    this.color = color;
+
+    this.limit = 300;
+    this.velocity = 0.08;
+  }
+
+  move(delta) {
+    // console.log(this.posX);
+    // console.log(this.limit)
+    this.posX += this.velocity * delta;
+    if (this.posX >= this.limit || this.posX <= 0) this.velocity = -this.velocity;
+    this.c.fillStyle = this.color;
+    this.c.fillRect(this.posX, this.posY, this.width, this.height);
+  }
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,10 @@
+/* global var */
+
 let requestId;
+let lastFrameTimeMs = 0;
+let maxFPS = 10;
+let delta = 0;
+/* end of global var */
 
 const canvas = {
   element: document.getElementById('canvas'),
@@ -15,26 +21,6 @@ const gameSetup = {
     this.setCanvasSize(720, 630);
   }
 };
-
-const loopControl = {
-  start() {
-    if (!requestId) {
-      requestId = window.requestAnimationFrame(update);
-      // return requestId;
-    }
-  },
-  stop() {
-    if (requestId) {
-      console.log('hello, stop');
-      window.cancelAnimationFrame(requestId);
-      requestId = undefined;
-    }
-  },
-  clear() {
-    canvas.context.clearRect(0, 0, canvas.element.width, canvas.element.height);
-  }
-};
-
 // check for positions and status from everything in canvas.
 const rules = {
   enemyArr: [],
@@ -60,7 +46,7 @@ const rules = {
     const isPlayerHit = this.enemyArr.some(e => {
       return player.isHit(e);
     });
-    console.log('is player hit? ', isPlayerHit);
+    // console.log('is player hit? ', isPlayerHit);
     return isPlayerHit;
     // console.log(loopControl.stop())
   },
@@ -69,9 +55,6 @@ const rules = {
 };
 
 /* calls */
-gameSetup.build();
-loopControl.start();
-
 const boss = rules.createBoss();
 
 const player = new Player(
@@ -82,6 +65,8 @@ const player = new Player(
   canvas.element.height - 250,
   'red'
 );
+
+const box = new Enemy(canvas.context, 50, 50, 100, 200, 'pink');
 
 const handleMoveInput = input => {
   switch (input.keyCode) {
@@ -111,23 +96,51 @@ const handleAttackInput = input => {
 document.addEventListener('keydown', handleMoveInput);
 document.addEventListener('keydown', handleAttackInput);
 
+// LOOP CONTROL AND UPDATE FUNCION
+const loopControl = {
+  start() {
+    if (!requestId) {
+      requestId = window.requestAnimationFrame(update);
+      // return requestId;
+    }
+  },
+  stop() {
+    if (requestId) {
+      console.log('hello, stop');
+      window.cancelAnimationFrame(requestId);
+      requestId = undefined;
+    }
+  },
+  clear() {
+    canvas.context.clearRect(0, 0, canvas.element.width, canvas.element.height);
+  }
+};
 
-/* MAIN GAME LOOP */
-function update() {
+function update(timestamp) {
   requestId = undefined;
   loopControl.clear();
-  // start
-  boss.draw();
-  player.draw();
-  // checa isAttacking? desliga eventListener de input e retorna uma funcao. Essa funcao recebe um array de funcoes e retorna uma delas. Essa funcao manipula o jogador, hitbox, etc...
+  if (timestamp < lastFrameTimeMs + (1000 / maxFPS)) {
+    loopControl.start();
+    return;
+  }
+  delta = timestamp - lastFrameTimeMs;
+  lastFrameTimeMs = timestamp;
+  // boss.draw();
+  // player.draw();
+  box.move(delta); // primeiro move, depois draw.
+  box.draw();
+  // TODO: checa isAttacking? desliga eventListener de input e retorna uma funcao. Essa funcao recebe um array de funcoes e retorna uma delas. Essa funcao manipula o jogador, hitbox, etc...
   if (rules.isGameover()) {
     console.log('game over!');
     loopControl.stop();
   } else {
-    loopControl.start();
+    loopControl.start(timestamp);
   }
   // end
 }
+gameSetup.build();
+
+loopControl.start();
 
 // setTimeout(loopControl.clear, 3000);
-setTimeout(loopControl.stop, 20000);
+// setTimeout(loopControl.stop, 5000);
