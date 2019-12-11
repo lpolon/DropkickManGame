8cea0c35d998ad69a3959ebbb038b48888d3f97a
Leonardo Polon <lpolon@gmail.com>
2019-12-10 21:33:26 -0300

    Player has new methods: check colision of enemies against attack hitbox. attackhitbox size is hardcoded in many places relative to player position. BEWARE!

--- javascript/Player.js
+++ javascript/Player.js
@@ -8,7 +8,7 @@ class Player extends Component {
     posX,
     posY,
     color,
-    facingDirection = 'left',
+    facingDirection = 'left'
   ) {
     super();
     this.c = context;
@@ -17,13 +17,12 @@ class Player extends Component {
     this.posX = posX;
     this.posY = posY;
     this.color = color;
+
     this.facingDirection = facingDirection;
     this.isAttacking = false;
   }
 
-  draw(color = this.color,
-    direction = this.facingDirection,
-    ) {
+  draw(color = this.color, direction = this.facingDirection) {
     this.c.fillStyle = color;
     this.c.fillRect(this.posX, this.posY, this.width, this.height);
     this.drawFacingDirection(direction);
@@ -31,23 +30,33 @@ class Player extends Component {
 
   drawFacingDirection(facingDirection) {
     if (facingDirection === 'right') {
-
       this.c.fillStyle = 'black';
-      this.c.fillRect(this.posX+19, this.posY +5, this.width - this.width / 2, this.height - this.height / 2);
+      this.c.fillRect(
+        this.posX + 19,
+        this.posY + 5,
+        this.width - this.width / 2,
+        this.height - this.height / 2
+      );
     } else if (facingDirection === 'left') {
       this.c.fillStyle = 'black';
-      this.c.fillRect(this.posX+1, this.posY +5, this.width - this.width / 2, this.height - this.height / 2);
+      this.c.fillRect(
+        this.posX + 1,
+        this.posY + 5,
+        this.width - this.width / 2,
+        this.height - this.height / 2
+      );
     }
   }
 
+  // *** PLAYER MOVEMENT ***
   goLeft() {
-    this.posX += -10;
     this.facingDirection = 'left';
+    this.posX += -10;
   }
 
   goRight() {
-    this.posX += 10;
     this.facingDirection = 'right';
+    this.posX += 10;
   }
 
   goTop() {
@@ -58,49 +67,107 @@ class Player extends Component {
     this.posY += 10;
   }
 
-  // TODO: attack function
+  // *** PLAYER HITBOX ***
+  _isRightCompBorderCollided(enemy) {
+    return this._rightCompHb() >= enemy._leftCompHb();
+  }
+
+  _isLeftCompBorderCollided(enemy) {
+    return this._leftCompHb() <= enemy._rightCompHb();
+  }
+
+  _isTopCompBorderCollided(enemy) {
+    return this._topCompHb() <= enemy._botCompHb();
+  }
+
+  _isBottomCompBorderCollided(enemy) {
+    return this._botCompHb() >= enemy._topCompHb();
+  }
+
+  isHitTaken(enemy) {
+    return (
+      this._isRightCompBorderCollided(enemy) &&
+      this._isLeftCompBorderCollided(enemy) &&
+      this._isTopCompBorderCollided(enemy) &&
+      this._isBottomCompBorderCollided(enemy)
+    );
+  }
+
+  // *** ATTACK STATE ***
   startAttack() {
     console.log('attack!');
     this.isAttacking = true;
   }
 
   stopAttack() {
-    console.log('hello, stopAttacking()')
+    console.log('hello, stopAttacking()');
     this.isAttacking = false;
   }
 
+  // *** ATTACK HITBOX ***
   drawAttackHitbox() {
     this.c.fillStyle = 'rebeccapurple';
     if (this.facingDirection === 'right') {
-
-      this.c.fillRect(this.posX + 10, this.posY-3, this.width, this.height-5);
+      this.c.fillRect(
+        this.posX + 10,
+        this.posY - 3,
+        this.width,
+        this.height - 5
+      );
     } else {
-      this.c.fillRect(this.posX - 10, this.posY-3, this.width, this.height-5);
+      this.c.fillRect(
+        this.posX - 10,
+        this.posY - 3,
+        this.width,
+        this.height - 5
+      );
     }
   }
 
-  _isRightBorderCollided(enemy) {
-    return this._rightCompHb() >= enemy._leftCompHb();
+  _leftAttackHb() {
+    if (this.facingDirection === 'right') {
+      return this.posX + 10;
+    }
+    return this.posX - 10;
   }
 
-  _isLeftBorderCollided(enemy) {
-    return this._leftCompHb() <= enemy._rightCompHb();
+  _rightAttackHb() {
+    if (this.facingDirection === 'right') {
+      return this.posX + 10 + this.width;
+    }
+    return this.posX - 10 + this.width;
   }
 
-  _isTopBorderCollided(enemy) {
-    return this._topCompHb() <= enemy._botCompHb();
+  _topAttackHb() {
+    return this.posY - 3;
   }
 
-  _isBottomBorderCollided(enemy) {
-    return this._botCompHb() >= enemy._topCompHb();
+  _botAttackHb() {
+    return this.posY - 3 + this.height - 5;
   }
 
-  isHitReceived(enemy) {
+  _isRightAttackBorderCollided(enemy) {
+    return this._rightAttackHb() >= enemy._leftCompHb();
+  }
+
+  _isLeftAttackBorderCollided(enemy) {
+    return this._leftAttackHb() <= enemy._rightCompHb();
+  }
+
+  _isTopAttackBorderCollided(enemy) {
+    return this._topAttackHb() <= enemy._botCompHb();
+  }
+
+  _isBottomAttackBorderCollided(enemy) {
+    return this._botAttackHb() >= enemy._topCompHb();
+  }
+
+  isHitGiven(enemy) {
     return (
-      this._isRightBorderCollided(enemy) &&
-      this._isLeftBorderCollided(enemy) &&
-      this._isTopBorderCollided(enemy) &&
-      this._isBottomBorderCollided(enemy)
+      this._isRightAttackBorderCollided(enemy) &&
+      this._isLeftAttackBorderCollided(enemy) &&
+      this._isTopAttackBorderCollided(enemy) &&
+      this._isBottomAttackBorderCollided(enemy)
     );
   }
 
